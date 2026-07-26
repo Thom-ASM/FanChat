e2246c87395e4393a68b393a453622adcb88d358
Thom-ASM <thomasmartin98@hotmail.com>
2026-07-26 21:38:15 +0100

    workflows

--- backend/terraform/backend.tf
+++ backend/terraform/backend.tf
@@ -1,9 +1,4 @@
 terraform {
   backend "s3" {
-    bucket       = "your-terraform-state-bucket"
-    key          = "${var.project_name}/terraform.tfstate"
-    region       = "eu-west-2"
-    encrypt      = true
-    use_lockfile = true
   }
 }
